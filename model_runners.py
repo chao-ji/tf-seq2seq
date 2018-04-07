1b602e6bd560c2bf0f114ef30f28c517d8170889
chao-ji <jic@indiana.edu>
2018-04-07 21:48:58 +0800

    Update model_runners.py

--- model_runners.py
+++ model_runners.py
@@ -5,12 +5,13 @@ import tensorflow as tf
 
 import data
 import model_helper
+import attention_model
 
 
 class _BaseModelRunner(object):
   mode = None
-  def __init__(self, builder, hparams):
-#    print(type(self).mode)
+  def __init__(self, builder, hparams, print_params=False):
+
     tf.contrib.learn.ModeKeys.validate(type(self).mode)
     self._graph = tf.Graph()
     with self._graph.as_default():
@@ -34,9 +35,12 @@ class _BaseModelRunner(object):
         self._global_step = tf.Variable(0, trainable=False, name="global_step")
       self._global_variables_initializer = tf.global_variables_initializer()
       
-      self._params = tf.trainable_variables()
+      self._params = tf.global_variables()
       self._saver = tf.train.Saver(
           self._params, max_to_keep=hparams.num_keep_ckpts)
+
+      if print_params:
+        model_helper.print_params(self)
 
   @property
   def graph(self):
@@ -57,15 +61,18 @@ class _BaseModelRunner(object):
 
     latest_ckpt = tf.train.latest_checkpoint(ckpt_dir)
     if latest_ckpt:
-      print("%s model is loading params from %s..." % (type(self).mode, latest_ckpt))
+      print("%s model is loading params from %s..." % (
+          type(self).mode.upper(), latest_ckpt))
       self._saver.restore(sess, latest_ckpt)
     else:
-      print("%s model is creating fresh params..." % type(self).mode)
+      print("%s model is creating fresh params..." % 
+          type(self).mode.upper())
       sess.run(self._global_variables_initializer)
 
-  def persist_params_to(self, sess, ckpt_dir):
-    print("%s model is saving params to %s" % (type(self).mode, ckpt_dir))
-    self._saver.save(sess, ckpt_dir)
+  def persist_params_to(self, sess, ckpt):
+    print("%s model is saving params to %s..." % (
+        type(self).mode.upper(), ckpt))
+    self._saver.save(sess, ckpt, global_step=self.global_step)
 
 
 class Seq2SeqModelTrainer(_BaseModelRunner):
@@ -79,16 +86,21 @@ class Seq2SeqModelTrainer(_BaseModelRunner):
       self.word_count = self.dataset.get_word_count()
       self.predict_count = self.dataset.get_predict_count()
       self.learning_rate = self._get_learning_rate(hparams)
-      self.update_op, self.grad_norm, grad_norm_summary = \
-          self._create_update_op(hparams)
+      self._loss = _compute_loss(self.dataset, self.model)
+      self.update_op, grad_norm_summary = self._create_update_op(hparams)
+
       self.train_summary = tf.summary.merge([
           tf.summary.scalar("lr", self.learning_rate),
-          tf.summary.scalar("train_loss", self.model.loss)
+          tf.summary.scalar("train_loss", self.loss)
       ] + grad_norm_summary)
       
   @property
   def global_step(self):
     return self._global_step
+
+  @property
+  def loss(self):
+    return self._loss
 
   def _get_learning_rate(self, hparams):
     learning_rate = tf.constant(hparams.learning_rate)
@@ -105,7 +117,7 @@ class Seq2SeqModelTrainer(_BaseModelRunner):
       inv_decay = warmup_factor**(
           tf.to_float(warmup_steps - self.global_step))
     else:
-      pass
+      raise ValueError("Unknown warmup scheme %s" % warmup_scheme) 
 
     return tf.cond(
         self.global_step < hparams.warmup_steps,
@@ -132,7 +144,7 @@ class Seq2SeqModelTrainer(_BaseModelRunner):
       decay_steps = 0
       decay_factor = 1.0
     elif hparams.decay_scheme:
-      pass
+      raise ValueError("Unknown decay scheme %s" % hparams.decay_scheme)
 
     return tf.cond(
         self.global_step < start_decay_step,
@@ -146,29 +158,28 @@ class Seq2SeqModelTrainer(_BaseModelRunner):
   def _create_update_op(self, hparams):
     if hparams.optimizer == "sgd":
       opt = tf.train.GradientDescentOptimizer(self.learning_rate)
-#      tf.summary.scalar("lr", self.learning_rate)
     elif hparams.optimizer == "adam":
       opt = tf.train.AdamOptimizer(self.learning_rate)
     else:
-      pass
+      raise ValueError("Unknown optimizer: %s" % hparams.optimizer) 
 
     params = self._params
     gradients = tf.gradients(
-        self.model.loss,
+        self.loss,
         params,
         colocate_gradients_with_ops=hparams.colocate_gradients_with_ops)
 
-    clipped_grads, grad_norm_summary, grad_norm = model_helper.gradient_clip(
-        gradients, hparams.max_gradient_norm)
+    clipped_grads, grad_norm_summary, self.grad_norm = \
+        model_helper.gradient_clip(gradients, hparams.max_gradient_norm)
 
     update_op = opt.apply_gradients(
         zip(clipped_grads, params), global_step=self.global_step)
 
-    return update_op, grad_norm, grad_norm_summary  
+    return update_op, grad_norm_summary
 
   def train(self, sess):
     return sess.run([self.update_op,
-                     self.model.loss,
+                     self.loss,
                      self.predict_count,
                      self.train_summary,
                      self.global_step,
@@ -178,7 +189,7 @@ class Seq2SeqModelTrainer(_BaseModelRunner):
                      self.learning_rate])
 
   def eval(self, sess):
-    return sess.run([self.model.loss,
+    return sess.run([self.loss,
                      self.predict_count,
                      self.model.batch_size])
 
@@ -193,9 +204,14 @@ class Seq2SeqModelEvaluator(_BaseModelRunner):
 
     with self.graph.as_default():
       self.predict_count = self.dataset.get_predict_count()
+      self._loss = _compute_loss(self.dataset, self.model)
+
+  @property
+  def loss(self):
+    return self._loss
 
   def eval(self, sess):
-    return sess.run([self.model.loss,
+    return sess.run([self.loss,
                      self.predict_count,
                      self.model.batch_size])
 
@@ -212,6 +228,17 @@ class Seq2SeqModelInferencer(_BaseModelRunner):
       self.sample_words = self.dataset.reverse_tgt_vocab_table.lookup(
           tf.to_int64(self.model.sample_id))
 
+      if hasattr(self.model.states, "alignment_history") and \
+          self.model.states.alignment_history:
+        attention_images = self.model.states.alignment_history.stack()
+        attention_images = tf.expand_dims(
+            tf.transpose(attention_images, [1, 2, 0]), -1)
+        attention_images *= 255
+        self.alignments_summary = tf.summary.image(
+            "attention_images", attention_images)
+      else:
+        self.alignments_summary = tf.no_op()
+
   @property
   def src_placeholder(self):
     return self._src_placeholder
@@ -221,14 +248,18 @@ class Seq2SeqModelInferencer(_BaseModelRunner):
     return self._batch_size_placeholder
 
   def infer(self, sess):
-    return sess.run([self.model.logits, self.model.sample_id, self.sample_words])
+    return sess.run([
+        self.model.logits,
+        self.alignments_summary,
+        self.model.sample_id,
+        self.sample_words])
 
   def decode(self, sess):
     # without beam search:
     # sample_words = [N, T] or [T, N]
     # with beam search:
     # sample_words = [N, T, B] or [T, N, B]
-    _, _, sample_words = self.infer(sess)
+    _, alignments_summary, _, sample_words = self.infer(sess)
 
     if self.model.time_major:
       sample_words = sample_words.transpose()
@@ -236,7 +267,29 @@ class Seq2SeqModelInferencer(_BaseModelRunner):
       sample_words = sample_words.transpose([2, 0, 1])
 
     # sample_words = [N, T] or [B, N, T]
-    return sample_words
+    return sample_words, alignments_summary
 
-  def _get_infer_summary(self):
-    pass
+
+def _compute_loss(dataset, model):
+  # tgt_output_ids = [N, T]
+  tgt_output_ids = dataset.tgt_output_ids
+  # tgt_output_ids = [T, N]
+  if model.time_major:
+    tgt_output_ids = tf.transpose(tgt_output_ids)
+  # xentropy = [N, T] or [T, N]     
+  xentropy = tf.nn.sparse_softmax_cross_entropy_with_logits(
+      labels=tgt_output_ids, logits=model.logits)
+
+  max_time = dataset.get_max_time_tgt()
+  # xentropy_weights = [N, T]
+  xentropy_weights = tf.sequence_mask(
+      dataset.tgt_seq_lens, max_time, dtype=tf.float32)
+  # xentropy_weights = [T, N]
+  if model.time_major:
+    xentropy_weights = tf.transpose(xentropy_weights)
+
+  # per sentence pair loss
+  loss = tf.reduce_sum(
+      xentropy * xentropy_weights) / tf.to_float(model.batch_size)
+
+  return loss
